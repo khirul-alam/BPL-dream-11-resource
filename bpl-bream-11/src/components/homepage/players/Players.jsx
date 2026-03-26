3cc6def66be45a259805358f825a083cd4fdb777
khirul-alame <yeasin1983@gmail.comm>
2026-03-26 21:56:37 +0600

    card

--- bpl-bream-11/src/components/homepage/players/Players.jsx
+++ bpl-bream-11/src/components/homepage/players/Players.jsx
@@ -1,14 +1,31 @@
+// import React, { use } from 'react';
+// import AvailablePlayers from '../../availablePlayers/AvailablePlayers';
+
+// const Players = ({fetchPlayer}) => {
+//     const player = use(fetchPlayer);
+//     console.log(player);
+    
+//     return (
+//         <div className='container mx-auto'>
+//             Players: {player.length}
+//             <AvailablePlayers Players={Players}></AvailablePlayers>
+//         </div>
+//     );
+// };
+
+// export default Players;
 import React, { use } from 'react';
 import AvailablePlayers from '../../availablePlayers/AvailablePlayers';
 
-const Players = ({fetchPlayer}) => {
-    const player = use(fetchPlayer);
-    console.log(player);
+const Players = ({ fetchPlayer }) => {
+    // use(fetchPlayer) দিয়ে ডেটা রেজলভ করা হচ্ছে
+    const playersData = use(fetchPlayer); 
     
     return (
-        <div className='container mx-auto'>
-            Players: {player.length}
-            <AvailablePlayers Players={Players}></AvailablePlayers>
+        <div className='container mx-auto py-10'>
+            <h2 className="text-2xl font-bold mb-5">Available Players: {playersData.length}</h2>
+            {/* এখানে সঠিক ডেটা 'playersData' পাঠানো হচ্ছে */}
+            <AvailablePlayers allPlayers={playersData}></AvailablePlayers>
         </div>
     );
 };
